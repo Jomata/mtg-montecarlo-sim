1c09abe8388bee3120cdee6b06723ede80d12018
JTP <jose.jtp@gmail.com>
2020-01-30 23:29:30 +0900

    Updates

---
+++ precache-manifest.135f433a744f661e39acdf4a77b65bff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e7f1a934cc9ec98101b123a5e33c49c",
+    "url": "/mtg-montecarlo-sim/index.html"
+  },
+  {
+    "revision": "575f662c4bb14c293721",
+    "url": "/mtg-montecarlo-sim/static/css/main.db5636c1.chunk.css"
+  },
+  {
+    "revision": "f62cfc501e4574f611d9",
+    "url": "/mtg-montecarlo-sim/static/js/2.ed197acf.chunk.js"
+  },
+  {
+    "revision": "da01034c70d479d890a70d42d48c6305",
+    "url": "/mtg-montecarlo-sim/static/js/2.ed197acf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "575f662c4bb14c293721",
+    "url": "/mtg-montecarlo-sim/static/js/main.6f781c5f.chunk.js"
+  },
+  {
+    "revision": "ce0874049bad28e7bfab",
+    "url": "/mtg-montecarlo-sim/static/js/runtime-main.30b849e0.js"
+  }
+]);
